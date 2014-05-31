faa1a821269c28566c699b5fa72b65d66afd507d
jahrichie <richardjosephoreilly@gmail.com>
2014-05-31 15:28:01 -0400

    first

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  # layout  :application
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+
+  def welcome
+    
+  end
+
+end
